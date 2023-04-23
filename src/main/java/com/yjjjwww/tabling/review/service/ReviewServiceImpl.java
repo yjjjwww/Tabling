bcd9a1aa925272ddcd1b3b68a054f124306faebe
yjjjwww <yjjjwww@naver.com>
2023-04-24 04:37:42 +0900

    feat : [015-REVIEW-LIST] 모든 리뷰 최신순 조회, 매장별 리뷰 최신순 조회 기능 구현

---
+++ src/main/java/com/yjjjwww/tabling/review/service/ReviewServiceImpl.java
@@ -0,0 +1,54 @@
+package com.yjjjwww.tabling.review.service;
+
+import com.yjjjwww.tabling.review.entity.Review;
+import com.yjjjwww.tabling.review.model.ReviewDto;
+import com.yjjjwww.tabling.review.repository.ReviewRepository;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ReviewServiceImpl implements ReviewService{
+
+    private final ReviewRepository reviewRepository;
+
+    @Override
+    public List<ReviewDto> getLatestReviewList() {
+        List<ReviewDto> result = new ArrayList<>();
+        List<Review> reviews = reviewRepository.findAllByOrderByCreatedAtDesc();
+        for (Review review : reviews) {
+            ReviewDto reviewDto = ReviewDto.builder()
+                .id(review.getId())
+                .contents(review.getContents())
+                .rating(review.getRating())
+                .restaurantName(review.getRestaurant().getName())
+                .customerUserId(review.getCustomer().getUserId())
+                .createdAt(review.getCreatedAt())
+                .updatedAt(review.getUpdatedAt())
+                .build();
+            result.add(reviewDto);
+        }
+        return result;
+    }
+
+    @Override
+    public List<ReviewDto> getReviewListByRestaurantId(Long restaurantId) {
+        List<ReviewDto> result = new ArrayList<>();
+        List<Review> reviews = reviewRepository.findAllByRestaurantIdOrderByCreatedAtDesc(restaurantId);
+        for (Review review : reviews) {
+            ReviewDto reviewDto = ReviewDto.builder()
+                .id(review.getId())
+                .contents(review.getContents())
+                .rating(review.getRating())
+                .restaurantName(review.getRestaurant().getName())
+                .customerUserId(review.getCustomer().getUserId())
+                .createdAt(review.getCreatedAt())
+                .updatedAt(review.getUpdatedAt())
+                .build();
+            result.add(reviewDto);
+        }
+        return result;
+    }
+}
